6a36cf950667e1e27e485df007bf7204849eb346
Soulan Saeng <ssaeng@digitalocean.com>
2025-07-04 10:20:55 -0400

    fix port

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// Port should come from environment variable or default to 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Game of Life app running at http://localhost:${PORT}`);
